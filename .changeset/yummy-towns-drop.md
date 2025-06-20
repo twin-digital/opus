40d53e53d135ae8237bdd6599df16b19e939f0bb
Sean Kleinjung <sean@twindigital.io>
2025-06-20 18:37:04 +0000

    repo-kit: update 'align-config' to only add dist entries to package.json files if there is a "src" folder

---
+++ .changeset/yummy-towns-drop.md
@@ -0,0 +1,5 @@
+---
+'@twin-digital/repo-kit': patch
+---
+
+update 'align-config' to only add dist entries to package.json files if there is a "src" folder
--- nodejs/devtools/repo-kit/src/cli/commands/align-config.ts
+++ nodejs/devtools/repo-kit/src/cli/commands/align-config.ts
@@ -1,70 +1,11 @@
-import fs from 'fs'
-import path from 'path'
 import { Command } from 'commander'
-import cloneDeep from 'lodash-es/cloneDeep.js'
-import isEqual from 'lodash-es/isEqual.js'
 import { getCurrentPackage } from '../../workspace/get-current-package.js'
 import type { PackageMeta } from '../../workspace/package-meta.js'
-
-const hasBarrelFile = (directory: string) =>
-  fs.existsSync(path.join(directory, 'index.ts'))
-
-const hasAnySubBarrelFiles = (directory: string) => {
-  const entries = fs.readdirSync(directory, { withFileTypes: true })
-  for (const entry of entries) {
-    if (
-      entry.isDirectory() &&
-      hasBarrelFile(path.join(directory, entry.name))
-    ) {
-      return true
-    }
-  }
-
-  return false
-}
-
-const alignPackageConfig = async (pkg: PackageMeta): Promise<void> => {
-  const newExports: Record<string, any> = {}
-
-  const srcDir = path.join(pkg.path, 'src')
-  if (fs.existsSync(srcDir) && fs.statSync(srcDir).isDirectory()) {
-    if (hasBarrelFile(srcDir)) {
-      newExports['.'] = {
-        import: './dist/index.js',
-        types: './dist/index.d.ts',
-      }
-    }
-
-    if (hasAnySubBarrelFiles(srcDir)) {
-      newExports['./*'] = {
-        import: './dist/*/index.js',
-        types: './dist/*/index.d.ts',
-      }
-    }
-  }
-
-  const newManifest = cloneDeep(pkg.manifest) as Record<string, any>
-  if (Object.keys(newExports).length > 0) {
-    newManifest.exports = newExports
-  } else {
-    delete newManifest.exports
-  }
-
-  newManifest.files = ['dist', '!*.d.ts.map']
-
-  if (!isEqual(pkg.manifest, newManifest)) {
-    const pkgJsonPath = path.join(pkg.path, 'package.json')
-    await fs.promises.writeFile(
-      pkgJsonPath,
-      JSON.stringify(newManifest, null, 2),
-      'utf-8',
-    )
-  }
-}
+import { makePackageJsonConfigPlugin } from '../../config-plugins/package-json.js'
 
 const handler = async () => {
   const pkg = await getCurrentPackage()
-  await alignPackageConfig(pkg)
+  await makePackageJsonConfigPlugin()(pkg)
 }
 
 export const makeCommand = () =>
