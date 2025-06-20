212598966582a5bff5461bd351bcf5b858607330
Sean Kleinjung <sean@twindigital.io>
2025-06-20 00:53:24 +0000

    repo-kit: extract markdown and workspace helpers from `update-readme` command

---
+++ nodejs/devtools/repo-kit/src/workspace/find-packages.ts
@@ -0,0 +1,60 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import type { ProjectManifest } from '@pnpm/types'
+import { execa } from 'execa'
+import { getWorkspaceRoot } from './get-workspace-root.js'
+
+export interface PackageMeta {
+  /**
+   * Manifest for the package (i.e. contents of its package.json)
+   */
+  manifest: ProjectManifest
+
+  /**
+   * Name of the package
+   */
+  name: string
+
+  /**
+   * Absolute path of the package
+   */
+  path: string
+}
+
+/**
+ * Finds all packages in the monorepo, and returns their name and path.
+ */
+export const findPackages = async ({
+  includeRoot = false,
+}: {
+  /**
+   * Whether the root package should be included or not.
+   * @defaultValue false
+   */
+  includeRoot?: boolean
+} = {}): Promise<PackageMeta[]> => {
+  const { stdout } = await execa({
+    encoding: 'utf8',
+  })`pnpm list -r --depth -1 --json`
+
+  const rootPath = await getWorkspaceRoot()
+  const allPackages = JSON.parse(stdout) as { name: string; path: string }[]
+  const packages =
+    includeRoot ? allPackages : (
+      allPackages.filter((pkg) => pkg.path !== rootPath)
+    )
+
+  return Promise.all(
+    packages.map(async (pkg) => {
+      const manifestPath = path.resolve(pkg.path, 'package.json')
+      const manifest = JSON.parse(
+        await fs.promises.readFile(manifestPath, 'utf-8'),
+      ) as ProjectManifest
+      return {
+        manifest,
+        name: pkg.name,
+        path: pkg.path,
+      }
+    }),
+  )
+}
