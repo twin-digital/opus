c002eecb1d9e69d8c53d5dc6c9b640009509aaea
Sean Kleinjung <sean@twindigital.io>
2025-06-24 19:30:30 +0000

    repo-kit: add utilities to load project configuration

---
+++ nodejs/devtools/repo-kit/src/configuration/README.md
@@ -0,0 +1,3 @@
+# repo-kit: src/configuration
+
+Defines types and functions for creating, reading, and applying user-supplied configuration of repo-kit tools.
---
+++ nodejs/devtools/repo-kit/src/configuration/configuration.ts
@@ -0,0 +1,12 @@
+/**
+ * Primitive values allowed in configuration objects.
+ */
+export type ConfigPrimitive = string | number | boolean | null | undefined
+
+/**
+ * A configuration value consisting of config primitives, or arrays or string-keyed records of the same.
+ */
+export type ConfigValue =
+  | ConfigPrimitive
+  | { [key: string]: ConfigValue }
+  | ConfigValue[]
