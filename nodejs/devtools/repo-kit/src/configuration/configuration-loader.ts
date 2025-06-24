3470312016647a1d24679053ce18e7d80ed111b6
Sean Kleinjung <sean@twindigital.io>
2025-06-24 19:30:30 +0000

    repo-kit: add utilities to load project configuration

---
+++ nodejs/devtools/repo-kit/src/configuration/configuration-loader.ts
@@ -0,0 +1,96 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import type { ConfigValue } from './configuration.js'
+import get from 'lodash-es/get.js'
+import yaml from 'yaml'
+
+/**
+ * Function which is able to load configuration from an implementation-specific source. Will return undefined if the
+ * underlying source of configuration data does not exist.
+ */
+export type ConfigurationLoaderFn = () =>
+  | ConfigValue
+  | Promise<ConfigValue>
+  | undefined
+  | Promise<undefined>
+
+/**
+ * Returns a `ConfigurationLoaderFn` which reads configuration from a specified file.
+ *
+ * @param file Absolute path to the config file
+ * @return A ConfigurationLoaderFn that returns the file content
+ */
+export const makeJsonFileLoader =
+  (file: string): ConfigurationLoaderFn =>
+  async () => {
+    if (fs.existsSync(file)) {
+      const content = await fs.promises.readFile(file, 'utf-8')
+      return JSON.parse(content) as ConfigValue
+    }
+
+    return undefined
+  }
+
+/**
+ * A configuration loader function which returns the value of a single value from the return value of another loader.
+ * @param key Key (or array of nested keys) of the config value to select.
+ */
+export const makeKeySelector =
+  (
+    key: string | string[],
+    delegateLoader: ConfigurationLoaderFn,
+  ): ConfigurationLoaderFn =>
+  async () => {
+    const allConfig = await delegateLoader()
+    return get(allConfig, key) as ConfigValue
+  }
+
+/**
+ * Creates a "chain" configuration loader which invokes a delegate loaders in order. The chain will return the first
+ * configuration value received which is not undefined. If all delegates return undefined, the chain will return
+ * undefined as well.
+ */
+export const makeLoaderChain =
+  (...loaders: ConfigurationLoaderFn[]): ConfigurationLoaderFn =>
+  async () => {
+    for (const loader of loaders) {
+      const result = await loader()
+      if (result !== undefined) {
+        return result
+      }
+    }
+
+    return undefined
+  }
+
+/**
+ * Creates a configuration loader which returns the value of a specific key from the `package.json` file found at the
+ * supplied path.
+ * @param key Key to return from the package.json file
+ * @param packagePath Package path, containing the package.json file
+ */
+export const makePackageJsonLoader = (
+  key: string | string[],
+  packagePath: string,
+): ConfigurationLoaderFn =>
+  makeKeySelector(
+    key,
+    makeJsonFileLoader(path.join(packagePath, 'package.json')),
+  )
+
+/**
+ * Returns a `ConfigurationLoaderFn` which reads configuration from a specified Yaml file.
+ *
+ * @param file Absolute path to the config file
+ * @return A ConfigurationLoaderFn that returns the file content
+ */
+export const makeYamlFileLoader =
+  (file: string): ConfigurationLoaderFn =>
+  async () => {
+    if (fs.existsSync(file)) {
+      const content = await fs.promises.readFile(file, 'utf-8')
+      return yaml.parse(content) as ConfigValue
+    }
+
+    return undefined
+  }
---
+++ nodejs/devtools/repo-kit/src/configuration/package-config-loader.ts
@@ -0,0 +1,39 @@
+import path from 'node:path'
+import {
+  makeJsonFileLoader,
+  makeLoaderChain,
+  makePackageJsonLoader,
+  makeYamlFileLoader,
+} from './configuration-loader.js'
+
+/**
+ * Returns a `ConfigurationLoaderFn` which reads configuration for a package from any of the following:
+ *
+ * - package.json, under the key `configName`
+ * - a yaml file called `.<CONFIG_NAME>.yaml` or `.<CONFIG_NAME>.yml`, in the package root
+ * - a json file called `.<CONFIG_NAME>.json` in the package root
+ *
+ * For example, given the `configName` "test-config" and packagePath "/home/test", config will be sourced from:
+ *
+ * - `/home/test/package.json`, from the key `configName`
+ * - `/home/test/.configName.yaml
+ * - `/home/test/.configName.yml
+ * - `/home/test/.configName.json
+ *
+ * The locations will be searched in the above order. The first value which exists will be used, and the other
+ * sources will be silently ignored.
+ *
+ * @param configName Name of the configuration to find, used to construct file names or package manifest keys
+ * @param packagePath Path of the package root, containing `package.json`
+ * @returns The loaded configuration, or undefined if no such configuration exists.
+ */
+export const makePackageConfigLoader = (
+  configName: string,
+  packagePath: string,
+) =>
+  makeLoaderChain(
+    makePackageJsonLoader(configName, packagePath),
+    makeYamlFileLoader(path.join(packagePath, `.${configName}.yaml`)),
+    makeYamlFileLoader(path.join(packagePath, `.${configName}.yml`)),
+    makeJsonFileLoader(path.join(packagePath, `.${configName}.json`)),
+  )
