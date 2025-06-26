e418d2d17d3cae25c369215823d7c7cd000cf87b
Sean Kleinjung <sean@twindigital.io>
2025-06-26 18:27:47 +0000

    create initial `SyncRule` factory
    - add 'exists' condition
    - add 'json merge patch' action
    - introduce necessary types for factory

---
+++ nodejs/devtools/repo-kit/src/sync/sync-rule-factory.ts
@@ -0,0 +1,110 @@
+import type { Configuration } from '../repo-kit-configuration.js'
+import type { PackageMeta } from '../workspace/package-meta.js'
+import { makeJsonMergePatchAction } from './actions/json-merge-patch.js'
+import { makeExistsCondition } from './conditions/exists-condition.js'
+import type { SyncResult } from './sync-result.js'
+import type { SyncRule } from './sync-rule.js'
+import type {
+  SyncRuleConfigEntry,
+  SyncRulesAction,
+  SyncRulesCondition,
+  SyncRulesConfig,
+} from './sync-rules-config.js'
+import some from 'lodash-es/some.js'
+
+/**
+ * Predicate (condition) used to determine if a particular rule applies to a given package.
+ */
+export type SyncRuleConditionFn = (
+  workspace: PackageMeta,
+) => boolean | Promise<boolean>
+
+/**
+ * Action which is taken by a rule to modify the contents of a package.
+ */
+export type SyncRuleActionFn = (
+  workspace: PackageMeta,
+) => SyncResult | Promise<SyncResult>
+
+const makeConditionFn = (condition: SyncRulesCondition) => {
+  return makeExistsCondition(condition.exists)
+}
+
+const makeActionFn = (action: SyncRulesAction<{ patch: string }>) => {
+  return makeJsonMergePatchAction(action.file, action.options)
+}
+
+const doApplyActions = async (
+  workspace: PackageMeta,
+  actions: SyncRuleActionFn[] = [],
+): Promise<SyncResult> => {
+  const changedFiles: string[] = []
+  for (const action of actions) {
+    const result = await action(workspace)
+    if (result.result === 'error') {
+      return result
+    } else if (result.result === 'ok') {
+      changedFiles.push(...result.changedFiles)
+    }
+  }
+
+  return changedFiles.length === 0 ?
+      { result: 'skipped' }
+    : {
+        changedFiles,
+        result: 'ok',
+      }
+}
+
+/**
+ * Create a sync rule to apply to a project.
+ * @param config The `SyncRulesConfigEntry` defining the conditions and actions for the rule
+ * @param userConfig User-supplied configuration to customize rule behavior.
+ * @returns The `SyncRule` corresponding to the supplied configuration.
+ */
+const makeSyncRule = (
+  config: SyncRuleConfigEntry,
+  _userConfig: Configuration,
+): SyncRule => {
+  const conditions = config.conditions.map((condition) =>
+    makeConditionFn(condition),
+  )
+  const applyActions = config.applyActions.map((action) =>
+    makeActionFn(action as SyncRulesAction<{ patch: string }>),
+  )
+  const unapplyActions = config.unapplyActions?.map((action) =>
+    makeActionFn(action as SyncRulesAction<{ patch: string }>),
+  )
+
+  return {
+    apply: (workspace: PackageMeta) => {
+      const isApplicable = some(conditions, (fn) => fn(workspace))
+
+      return isApplicable ?
+          doApplyActions(workspace, applyActions)
+        : doApplyActions(workspace, unapplyActions)
+    },
+    name: config.name,
+  }
+}
+
+/**
+ * Create the sync rules to apply to a project.
+ * @param config The `SyncRulesConfig` defining the conditions and actions for each rule.
+ * @param userConfig User-supplied configuration to customize rule behavior.
+ * @returns Set of `SyncRules` applicable to the supplied configuration.
+ */
+export const makeSyncRules = ({
+  config,
+  rules,
+}: {
+  /**
+   * User-supplied repo-kit configuration used to customize how rules are applied.
+   */
+  config: Configuration
+
+  /**
+   * The `SyncRules` config defining the available rules.
+   */
+  rules: SyncRulesConfig
+}): SyncRule[] => rules.syncRules.map((rule) => makeSyncRule(rule, config))
--- nodejs/devtools/repo-kit/src/sync/sync-rule.ts
+++ nodejs/devtools/repo-kit/src/sync/sync-rule.ts
@@ -1,112 +1,20 @@
-import { some } from 'lodash-es'
-import { makeJsonMergePatchAction } from './actions/json-merge-patch.js'
-import { makeExistsCondition } from './conditions/exists-condition.js'
-import type {
-  SyncRuleActionFn,
-  SyncRuleConditionFn,
-} from './legacy-sync-rule.js'
-import type {
-  SyncRuleConfigEntry,
-  SyncRulesAction,
-  SyncRulesCondition,
-  SyncRulesConfig,
-} from './sync-rules-config.js'
 import type { PackageMeta } from '../workspace/package-meta.js'
-import { type SyncResult } from './sync-result.js'
-import type { Configuration } from '../repo-kit-configuration.js'
+import type { SyncResult } from './sync-result.js'
 
-export interface SyncOptions {
+/**
+ * A `SyncRule` represents one or more changes to a monorepo package that are applied each workspace.
+ */
+export interface SyncRule {
   /**
-   * The repo-kit configuration which applies to the package.
-   */
-  configuration: Configuration
-}
-
-export class SyncRule {
-  private applyActions: SyncRuleActionFn[] = []
-  private conditions: SyncRuleConditionFn[] = []
-  private _name: string
-  private unapplyActions: SyncRuleActionFn[] = []
-
-  public static fromConfig(config: SyncRuleConfigEntry) {
-    const that = new SyncRule(config.name)
-    config.conditions.forEach((condition) => {
-      that.addCondition(condition)
-    })
-    config.applyActions.forEach((action) => {
-      that.addApplyAction(action as SyncRulesAction<{ patch: string }>)
-    })
-    config.unapplyActions?.forEach((action) => {
-      that.addUnapplyAction(action as SyncRulesAction<{ patch: string }>)
-    })
-
-    return that
-  }
-
-  private async doApplyActions(
-    workspace: PackageMeta,
-    options: SyncOptions,
-    actions: SyncRuleActionFn[] = [],
-  ): Promise<SyncResult> {
-    const changedFiles: string[] = []
-    for (const action of actions) {
-      const result = await action(workspace, options)
-      if (result.result === 'error') {
-        return result
-      } else if (result.result === 'ok') {
-        changedFiles.push(...result.changedFiles)
-      }
-    }
-
-    return changedFiles.length === 0 ?
-        { result: 'skipped' }
-      : {
-          changedFiles,
-          result: 'ok',
-        }
-  }
-
-  private constructor(name: string) {
-    this._name = name
-  }
-
-  private addApplyAction(config: SyncRulesAction<{ patch: string }>) {
-    const action = makeJsonMergePatchAction(config.file, config.options)
-    this.applyActions.push(action)
-  }
-
-  private addCondition(config: SyncRulesCondition) {
-    const condition = makeExistsCondition(config.exists)
-    this.conditions.push(condition)
-  }
-
-  private addUnapplyAction(config: SyncRulesAction<{ patch: string }>) {
-    const action = makeJsonMergePatchAction(config.file, config.options)
-    this.unapplyActions.push(action)
-  }
-
-  /**
-   * Apply this rule to a given workspace. If the workspace matches any condition, the 'applyActions' are executed.
-   * Otherwise, the 'unapplyActions' are executed instead.
+   * Applies the changes associated with this rule to an applicable workspace. Typically, changes including adding,
+   * removing, or updating files in the workspace.
    *
    * @param workspace Metadata for the package to which the rule will be applied.
-   * @param options Execution context, containing the user-supplied configuration and other global metadata.
    */
-  public apply(
-    workspace: PackageMeta,
-    options: SyncOptions,
-  ): Promise<SyncResult> {
-    const isApplicable = some(this.conditions, (fn) => fn(workspace, options))
+  apply(workspace: PackageMeta): SyncResult | Promise<SyncResult>
 
-    return isApplicable ?
-        this.doApplyActions(workspace, options, this.applyActions)
-      : this.doApplyActions(workspace, options, this.unapplyActions)
-  }
-
-  public get name(): string {
-    return this._name
-  }
+  /**
+   * Human-readable name for this rule.
+   */
+  name: string
 }
-
-export const makeSyncRules = (config: SyncRulesConfig): SyncRule[] =>
-  config.syncRules.map((rule) => SyncRule.fromConfig(rule))
