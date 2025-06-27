e637b16decb8d797f39506c49304cd5a393910d2
Sean Kleinjung <sean@twindigital.io>
2025-06-27 00:48:11 +0000

    normalize how disabling rules via configuration works

--- nodejs/devtools/repo-kit/src/sync/legacy-plugins/eslint-config-bootstrap.ts
+++ nodejs/devtools/repo-kit/src/sync/legacy-plugins/eslint-config-bootstrap.ts
@@ -1,6 +1,5 @@
 import type { LegacySyncPlugin } from '../legacy-sync-plugin.js'
 import { makeConfigPlugin } from '../legacy-make-config-plugin.js'
-import type { Configuration } from '../../repo-kit-configuration.js'
 
 const bootstrapEslintConfig = `import base from '@twin-digital/eslint-config'
 
@@ -12,13 +11,9 @@ export default base
  * it returns the original content unchanged. This plugin can be used to ensure that new projects are properly
  * initialized without overwriting any package-specific configuration.
  */
-export const makeEslintBootstrapPlugin = ({
-  eslint,
-}: Configuration): LegacySyncPlugin | undefined =>
-  eslint ?
-    makeConfigPlugin('eslint-config-bootstrap', {
-      'eslint.config.js': (content) => {
-        return content ?? bootstrapEslintConfig
-      },
-    })
-  : undefined
+export const makeEslintBootstrapPlugin = (): LegacySyncPlugin | undefined =>
+  makeConfigPlugin('eslint-config-bootstrap', {
+    'eslint.config.js': (content) => {
+      return content ?? bootstrapEslintConfig
+    },
+  })
